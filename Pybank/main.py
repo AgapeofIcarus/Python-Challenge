ae94990994aa53574afdf2f7df7b81e8416c8f43
Chase Webb <72991599+AgapeofIcarus@users.noreply.github.com>
2021-01-13 00:56:38 -0500

    Update main.py
    reworked coding

--- Pybank/main.py
+++ Pybank/main.py
@@ -4,6 +4,16 @@ import csv
 
 csvpath = os.path.join('..', '..', 'Homework', 'python-challenge', 'budget_data.csv')
 
+#Set Definitions
+total_months = []
+total_profit_changes = []
+count_months = 0
+total_profits_losses = 0
+previous_loss_gain = 0
+current_loss_gain = 0
+profit_change = 0
+
+#open and read csvfile
 with open(csvpath) as csvfile:
 
     csvreader = csv.reader(csvfile, delimiter=',')
@@ -16,40 +26,46 @@ with open(csvpath) as csvfile:
     for row in csvreader:
         print(row)
 
-#Set definitions
-total_months = 0
-total_profit = 0
-prior_profit = 0
-months_with_change = []
-profitchange = []
-greatest_increase = ["", 0]
-greatest_decrease = ["", 9999999]
-average_change = 0
+        #Count total months
+        count_months += 1
 
-#Calculate total months, net total, profit changes, greatest increase, and greatest decrease.
-def data_set(path):
-    with open(csvpath) as csvfile:
-        csvreader = csv.DictReader(csvfile, delimiter=',')
-        header = next(csvreader)
-    for row in csvreader:
-        yield row
+        #Calculate net total of profits/losses
+        current_loss_gain = int(row[1])
+        total_profits_losses += current_loss_gain
 
-for row in (data_set(csvpath)):
-    #calculate total months
-    total_months = total_months + 1
+        if (count_months == 1):
+            previous_loss_gain = current_loss_gain
+            continue
 
-    #calculate net total
-    total_profit = total_profit + int(row["Profit/Losses"])
-    
-    #calculate profit changes
-    prior_profit = int(row["Profit/Losses"])
-    average_change = int(row["Profit/Losses"] - prior_profit)
-    profit_change = profit_change - [average_change]
+        else:
+            profit_change = current_loss_gain - previous_loss_gain
 
-    #Define months with greatest and least change
-    months_with_change = months_with_change + [row["Date"]]
-    if (average_change > greatest_increase[1]):
-        greatest_increase[0] = row["Date"]
-        greatest_increase[1] = average_change
-    if (average_change < greatest_decrease[1]):
-        
+            #Append Month and Profit data
+            total_months.append(row[0])
+            total_profit_changes.append(profit_change)
+
+            #Prepare loss data for next loop
+            previous_loss_gain = current_loss_gain
+
+    #Total and average figures for profits/losses
+    profit_changes_sum = sum(total_profit_changes)
+    profit_changes_average = round(profit_changes_sum/(count_months - 1), 2)
+
+    #Generate greatest increase and decrease of profits/losses
+    profit_increase = max(total_profit_changes)
+    loss_decrease = min(total_profit_changes)
+
+    #Pull index information for profit max and min increase/decrease data, then assign to their given months.
+    greatest_increase_index = total_profit_changes.index(profit_increase)
+    greatest_decrease_index = total_profit_changes.index(loss_decrease)
+    month_maximum_increase = total_months[greatest_increase_index]
+    month_maximum_decrease = total_months[greatest_decrease_index]
+
+    #Print Financial Analysis
+    print("Financial Analysis")
+    print("---------------------")
+    print(f"Total Months: {count_months}")
+    print(f"Total: ${total_profits_losses}")
+    print(f"Average Change: ${profit_changes_average}")
+    print(f"Greatest Increase In Profits: {month_maximum_increase} ${profit_increase}")
+    print(f"Greatest Decrease In Losses: {month_maximum_decrease} ${loss_decrease}")
