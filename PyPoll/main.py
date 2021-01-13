aedf695cc37c63d08407ce8f99733bd312cd3f1a
Chase Webb <72991599+AgapeofIcarus@users.noreply.github.com>
2021-01-13 02:26:00 -0500

    Update main.py
    added code for counts and corrected errors

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -9,7 +9,7 @@ election_data_csv_path = os.path.join("Resources", "election_data.csv")
 
 #Set defintions
 candidates = []
-voters_per = []
+votes_per = []
 
 with open(election_data_csv_path, newline="") as csvfile:
     csv_reader = csv.reader(csvfile, delimiter=",")
@@ -19,10 +19,13 @@ with open(election_data_csv_path, newline="") as csvfile:
     for row in csv_reader:
         candidates.append(row[2])
 
-        #create a sorted list for candidates
-        sorted_list = sorted(candidates)
-        arrange_list = sorted_list
+    #create a sorted list for candidates
+    sorted_list = sorted(candidates)
+    arrange_list = sorted_list
 
-        #count candidates using counter
-        count_candidates= collections.Counter(arrange_list)
-        print(count_candidates)
+    #count candidates using counter
+    count_candidates= collections.Counter(arrange_list)
+    votes_per.append(count_candidates.most_common())
+
+    #generate percentage of votes for each candidate
+    for item in votes_per:
